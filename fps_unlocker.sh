01a686f44296aa5e225a8a2bc6d5be062a4b8196
Henri <159345046+Henrit14@users.noreply.github.com>
2025-10-04 19:13:27 -0400

    Update fps_unlocker.sh

--- fps_unlocker.sh
+++ fps_unlocker.sh
@@ -6,6 +6,7 @@ IXP_FILE="$CLIENT_SETTINGS/IxpSettings.json"
 XML_FILE="$ROBLOX_DIR/GlobalBasicSettings_13.xml"
 
 mkdir -p "$ROBLOX_DIR"
+read -p "Please enter a fps value: " x
 
 if [ ! -d "$CLIENT_SETTINGS" ]; then
     mkdir "$CLIENT_SETTINGS"
@@ -18,7 +19,7 @@ fi
 cat > "$IXP_FILE" <<EOL
 {
     "FFlagTaskSchedulerLimitTargetFpsTo2402": "False",
-    "DFIntTaskSchedulerTargetFps": "120",
+    "DFIntTaskSchedulerTargetFps": $x,
     "FFlagGameBasicSettingsFramerateCap5": "True",
     "FFlagDebugGraphicsDisableMetal": "True",
     "FFlagDebugGraphicsPreferVulkan": "False"
@@ -31,4 +32,4 @@ if [ -f "$XML_FILE" ]; then
     sed -i '' 's/\(<int name="FramerateCap" value="\)[^"]*"/\1-1"/' "$XML_FILE"
 fi
 
-echo "Roblox FPS Unlocked! | If you have any issues DM @omaw on Discord"
+echo "Roblox FPS Unlocked! |credits to @omaw on dc, sub to @hxnrirblx"
